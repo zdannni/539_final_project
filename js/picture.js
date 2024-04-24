190b1140074ceca6a9f3ec644e57803ba72bb1e8
Zhibo Li <127414291+zdannni@users.noreply.github.com>
2024-04-23 22:21:55 -0400

    Add files via upload

--- js/picture.js
+++ js/picture.js
@@ -20,7 +20,7 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
 }
 
-/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+
 function myFunction() {
   var x = document.getElementById("myLinks");
   if (x.style.display === "block") {
